9681d1cce42512ede05c5bbd93637863aec7dce2
Fred Prates <fredprates@outlook.com>
2017-04-29 18:27:27 -0300

    Layout novo

--- ProtProg/Principal.Designer.cs
+++ ProtProg/Principal.Designer.cs
@@ -96,15 +96,15 @@
             this.panel1.Controls.Add(this.cmd1);
             this.panel1.Location = new System.Drawing.Point(35, 50);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(60, 287);
+            this.panel1.Size = new System.Drawing.Size(112, 386);
             this.panel1.TabIndex = 2;
             // 
             // cmd5
             // 
             this.cmd5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.cmd5.Location = new System.Drawing.Point(3, 227);
+            this.cmd5.Location = new System.Drawing.Point(22, 311);
             this.cmd5.Name = "cmd5";
-            this.cmd5.Size = new System.Drawing.Size(53, 52);
+            this.cmd5.Size = new System.Drawing.Size(70, 70);
             this.cmd5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.cmd5.TabIndex = 5;
             this.cmd5.TabStop = false;
@@ -112,9 +112,9 @@
             // cmd4
             // 
             this.cmd4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.cmd4.Location = new System.Drawing.Point(3, 171);
+            this.cmd4.Location = new System.Drawing.Point(22, 234);
             this.cmd4.Name = "cmd4";
-            this.cmd4.Size = new System.Drawing.Size(53, 52);
+            this.cmd4.Size = new System.Drawing.Size(70, 70);
             this.cmd4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.cmd4.TabIndex = 5;
             this.cmd4.TabStop = false;
@@ -122,9 +122,9 @@
             // cmd3
             // 
             this.cmd3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.cmd3.Location = new System.Drawing.Point(3, 115);
+            this.cmd3.Location = new System.Drawing.Point(22, 158);
             this.cmd3.Name = "cmd3";
-            this.cmd3.Size = new System.Drawing.Size(53, 52);
+            this.cmd3.Size = new System.Drawing.Size(70, 70);
             this.cmd3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.cmd3.TabIndex = 3;
             this.cmd3.TabStop = false;
@@ -132,9 +132,9 @@
             // cmd2
             // 
             this.cmd2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.cmd2.Location = new System.Drawing.Point(3, 59);
+            this.cmd2.Location = new System.Drawing.Point(22, 82);
             this.cmd2.Name = "cmd2";
-            this.cmd2.Size = new System.Drawing.Size(53, 52);
+            this.cmd2.Size = new System.Drawing.Size(70, 70);
             this.cmd2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.cmd2.TabIndex = 2;
             this.cmd2.TabStop = false;
@@ -142,26 +142,26 @@
             // cmd1
             // 
             this.cmd1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.cmd1.Location = new System.Drawing.Point(3, 3);
+            this.cmd1.Location = new System.Drawing.Point(22, 6);
             this.cmd1.Name = "cmd1";
-            this.cmd1.Size = new System.Drawing.Size(53, 52);
+            this.cmd1.Size = new System.Drawing.Size(70, 70);
             this.cmd1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.cmd1.TabIndex = 1;
             this.cmd1.TabStop = false;
             // 
             // TB_lit
             // 
-            this.TB_lit.Location = new System.Drawing.Point(381, 12);
+            this.TB_lit.Location = new System.Drawing.Point(739, 12);
             this.TB_lit.Name = "TB_lit";
             this.TB_lit.ReadOnly = true;
-            this.TB_lit.Size = new System.Drawing.Size(188, 219);
+            this.TB_lit.Size = new System.Drawing.Size(188, 319);
             this.TB_lit.TabIndex = 5;
             this.TB_lit.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(48, 33);
+            this.label1.Location = new System.Drawing.Point(32, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 6;
@@ -170,7 +170,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 340);
+            this.label2.Location = new System.Drawing.Point(32, 439);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(23, 13);
             this.label2.TabIndex = 7;
@@ -178,7 +178,7 @@
             // 
             // Bt_Literal
             // 
-            this.Bt_Literal.Location = new System.Drawing.Point(381, 239);
+            this.Bt_Literal.Location = new System.Drawing.Point(739, 337);
             this.Bt_Literal.Name = "Bt_Literal";
             this.Bt_Literal.Size = new System.Drawing.Size(91, 41);
             this.Bt_Literal.TabIndex = 8;
@@ -194,15 +194,15 @@
             this.panel2.Controls.Add(this.panel1);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.label2);
-            this.panel2.Location = new System.Drawing.Point(219, 12);
+            this.panel2.Location = new System.Drawing.Point(541, 12);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(136, 358);
+            this.panel2.Size = new System.Drawing.Size(192, 454);
             this.panel2.TabIndex = 9;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(19, 9);
+            this.label4.Location = new System.Drawing.Point(3, 9);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(90, 13);
             this.label4.TabIndex = 6;
@@ -217,9 +217,9 @@
             this.panel3.Controls.Add(this.moveH);
             this.panel3.Controls.Add(this.moveAH);
             this.panel3.Controls.Add(this.LoopPb);
-            this.panel3.Location = new System.Drawing.Point(12, 12);
+            this.panel3.Location = new System.Drawing.Point(307, 12);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(182, 358);
+            this.panel3.Size = new System.Drawing.Size(228, 453);
             this.panel3.TabIndex = 10;
             // 
             // label3
@@ -235,9 +235,9 @@
             // 
             this.moveF.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.moveF.Image = global::ProtProg.Properties.Resources.reto2;
-            this.moveF.Location = new System.Drawing.Point(61, 94);
+            this.moveF.Location = new System.Drawing.Point(77, 108);
             this.moveF.Name = "moveF";
-            this.moveF.Size = new System.Drawing.Size(53, 56);
+            this.moveF.Size = new System.Drawing.Size(70, 70);
             this.moveF.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.moveF.TabIndex = 0;
             this.moveF.TabStop = false;
@@ -250,9 +250,9 @@
             // 
             this.moveH.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.moveH.Image = global::ProtProg.Properties.Resources.gir_horario;
-            this.moveH.Location = new System.Drawing.Point(113, 150);
+            this.moveH.Location = new System.Drawing.Point(146, 179);
             this.moveH.Name = "moveH";
-            this.moveH.Size = new System.Drawing.Size(53, 56);
+            this.moveH.Size = new System.Drawing.Size(70, 70);
             this.moveH.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.moveH.TabIndex = 3;
             this.moveH.TabStop = false;
@@ -265,9 +265,9 @@
             // 
             this.moveAH.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.moveAH.Image = global::ProtProg.Properties.Resources.gir_anti_horario;
-            this.moveAH.Location = new System.Drawing.Point(9, 150);
+            this.moveAH.Location = new System.Drawing.Point(7, 178);
             this.moveAH.Name = "moveAH";
-            this.moveAH.Size = new System.Drawing.Size(53, 56);
+            this.moveAH.Size = new System.Drawing.Size(70, 70);
             this.moveAH.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.moveAH.TabIndex = 4;
             this.moveAH.TabStop = false;
@@ -280,9 +280,9 @@
             // 
             this.LoopPb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.LoopPb.Image = ((System.Drawing.Image)(resources.GetObject("LoopPb.Image")));
-            this.LoopPb.Location = new System.Drawing.Point(61, 206);
+            this.LoopPb.Location = new System.Drawing.Point(77, 251);
             this.LoopPb.Name = "LoopPb";
-            this.LoopPb.Size = new System.Drawing.Size(53, 56);
+            this.LoopPb.Size = new System.Drawing.Size(70, 70);
             this.LoopPb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.LoopPb.TabIndex = 7;
             this.LoopPb.TabStop = false;
@@ -293,7 +293,7 @@
             // 
             // Bt_Limpar
             // 
-            this.Bt_Limpar.Location = new System.Drawing.Point(478, 239);
+            this.Bt_Limpar.Location = new System.Drawing.Point(836, 337);
             this.Bt_Limpar.Name = "Bt_Limpar";
             this.Bt_Limpar.Size = new System.Drawing.Size(91, 41);
             this.Bt_Limpar.TabIndex = 11;
@@ -303,7 +303,7 @@
             // 
             // Bt_Enviar
             // 
-            this.Bt_Enviar.Location = new System.Drawing.Point(381, 327);
+            this.Bt_Enviar.Location = new System.Drawing.Point(739, 425);
             this.Bt_Enviar.Name = "Bt_Enviar";
             this.Bt_Enviar.Size = new System.Drawing.Size(188, 41);
             this.Bt_Enviar.TabIndex = 12;
@@ -320,14 +320,14 @@
             this.panel4.Controls.Add(this.label5);
             this.panel4.Controls.Add(this.numericUpDown1);
             this.panel4.Controls.Add(this.panel5);
-            this.panel4.Location = new System.Drawing.Point(12, 395);
+            this.panel4.Location = new System.Drawing.Point(12, 12);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(557, 129);
+            this.panel4.Size = new System.Drawing.Size(289, 453);
             this.panel4.TabIndex = 14;
             // 
             // Bt_Gerar
             // 
-            this.Bt_Gerar.Location = new System.Drawing.Point(395, 38);
+            this.Bt_Gerar.Location = new System.Drawing.Point(3, 271);
             this.Bt_Gerar.Name = "Bt_Gerar";
             this.Bt_Gerar.Size = new System.Drawing.Size(139, 62);
             this.Bt_Gerar.TabIndex = 15;
@@ -338,7 +338,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(114, 21);
+            this.label7.Location = new System.Drawing.Point(153, 13);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(101, 13);
             this.label7.TabIndex = 18;
@@ -347,7 +347,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(327, 22);
+            this.label6.Location = new System.Drawing.Point(50, 172);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(42, 13);
             this.label6.TabIndex = 17;
@@ -365,7 +365,7 @@
             // numericUpDown1
             // 
             this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown1.Location = new System.Drawing.Point(323, 37);
+            this.numericUpDown1.Location = new System.Drawing.Point(27, 188);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             5,
             0,
@@ -378,7 +378,7 @@
             0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.ReadOnly = true;
-            this.numericUpDown1.Size = new System.Drawing.Size(57, 62);
+            this.numericUpDown1.Size = new System.Drawing.Size(91, 62);
             this.numericUpDown1.TabIndex = 16;
             this.numericUpDown1.Value = new decimal(new int[] {
             1,
@@ -395,17 +395,17 @@
             this.panel5.Controls.Add(this.LoopPcBox3);
             this.panel5.Controls.Add(this.LoopPcBox4);
             this.panel5.Controls.Add(this.LoopPcBox1);
-            this.panel5.Location = new System.Drawing.Point(18, 37);
+            this.panel5.Location = new System.Drawing.Point(146, 29);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(299, 62);
+            this.panel5.Size = new System.Drawing.Size(108, 403);
             this.panel5.TabIndex = 15;
             // 
             // LoopPcBox2
             // 
             this.LoopPcBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.LoopPcBox2.Location = new System.Drawing.Point(62, 3);
+            this.LoopPcBox2.Location = new System.Drawing.Point(17, 95);
             this.LoopPcBox2.Name = "LoopPcBox2";
-            this.LoopPcBox2.Size = new System.Drawing.Size(53, 52);
+            this.LoopPcBox2.Size = new System.Drawing.Size(70, 70);
             this.LoopPcBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.LoopPcBox2.TabIndex = 5;
             this.LoopPcBox2.TabStop = false;
@@ -413,9 +413,9 @@
             // LoopPcBox5
             // 
             this.LoopPcBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.LoopPcBox5.Location = new System.Drawing.Point(239, 3);
+            this.LoopPcBox5.Location = new System.Drawing.Point(17, 323);
             this.LoopPcBox5.Name = "LoopPcBox5";
-            this.LoopPcBox5.Size = new System.Drawing.Size(53, 52);
+            this.LoopPcBox5.Size = new System.Drawing.Size(70, 70);
             this.LoopPcBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.LoopPcBox5.TabIndex = 5;
             this.LoopPcBox5.TabStop = false;
@@ -423,9 +423,9 @@
             // LoopPcBox3
             // 
             this.LoopPcBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.LoopPcBox3.Location = new System.Drawing.Point(121, 3);
+            this.LoopPcBox3.Location = new System.Drawing.Point(17, 171);
             this.LoopPcBox3.Name = "LoopPcBox3";
-            this.LoopPcBox3.Size = new System.Drawing.Size(53, 52);
+            this.LoopPcBox3.Size = new System.Drawing.Size(70, 70);
             this.LoopPcBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.LoopPcBox3.TabIndex = 3;
             this.LoopPcBox3.TabStop = false;
@@ -433,9 +433,9 @@
             // LoopPcBox4
             // 
             this.LoopPcBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.LoopPcBox4.Location = new System.Drawing.Point(180, 3);
+            this.LoopPcBox4.Location = new System.Drawing.Point(17, 247);
             this.LoopPcBox4.Name = "LoopPcBox4";
-            this.LoopPcBox4.Size = new System.Drawing.Size(53, 52);
+            this.LoopPcBox4.Size = new System.Drawing.Size(70, 70);
             this.LoopPcBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.LoopPcBox4.TabIndex = 2;
             this.LoopPcBox4.TabStop = false;
@@ -443,16 +443,16 @@
             // LoopPcBox1
             // 
             this.LoopPcBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.LoopPcBox1.Location = new System.Drawing.Point(3, 3);
+            this.LoopPcBox1.Location = new System.Drawing.Point(17, 19);
             this.LoopPcBox1.Name = "LoopPcBox1";
-            this.LoopPcBox1.Size = new System.Drawing.Size(53, 52);
+            this.LoopPcBox1.Size = new System.Drawing.Size(70, 70);
             this.LoopPcBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.LoopPcBox1.TabIndex = 1;
             this.LoopPcBox1.TabStop = false;
             // 
             // Bt_Conexao
             // 
-            this.Bt_Conexao.Location = new System.Drawing.Point(381, 283);
+            this.Bt_Conexao.Location = new System.Drawing.Point(739, 381);
             this.Bt_Conexao.Name = "Bt_Conexao";
             this.Bt_Conexao.Size = new System.Drawing.Size(188, 41);
             this.Bt_Conexao.TabIndex = 15;
@@ -472,7 +472,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.ClientSize = new System.Drawing.Size(584, 536);
+            this.ClientSize = new System.Drawing.Size(938, 475);
             this.Controls.Add(this.Bt_Conexao);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.Bt_Enviar);
